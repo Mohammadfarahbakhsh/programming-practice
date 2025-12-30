9e8097bb6c7dca844a7c170d8998a9b1a9830cca
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-12-30 23:13:19 +0330

    solving quera question

---
+++ python/Alpha Centauri.py
@@ -0,0 +1,7 @@
+finger=int(input("enter number of your finger: "))
+base=int(input("enter base: "))
+def AlphaCentauri():
+    result=finger//base
+    result1=finger%base
+    print(result,result1)
+AlphaCentauri()
